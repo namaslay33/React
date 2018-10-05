b54a68f018a79a443b8045626021d73e678179ed
Namaslay33 <skylar.hunt@utexas.edu>
2018-10-05 09:52:36 -0500

    heroku test

--- MemoryGame/src/MemoryCard.js
+++ MemoryGame/src/MemoryCard.js
@@ -3,10 +3,19 @@ import './App.css';
 import MemoryCard from './MemoryCard.css'
 
 class App extends React.Component{
+    clickHandler(){
+        alert("card clicked");
+    }
     render(){
         return(
             <div className="MemoryCard">
-                <img src={"https://www.digitalcrafts.com/img/DigitalCrafts-Logo-Wrench.png"}/>
+                <div className="MemoryCardInner">
+                    <div className="MemoryCardBack">
+                        <img src={"https://www.digitalcrafts.com/img/DigitalCrafts-Logo-Wrench.png"}/>
+                    </div>
+                    <div className="MemoryCardFront">∆
+                    </div>
+                </div>
             </div>
         );
     }
